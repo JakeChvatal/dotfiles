372289fb7a28d2a6c515572d827815dcbc192cdc
jakechvatal <jakechvatal@github.com>
2020-04-22 12:15:50 -0400

    fixed for three monitor support

--- emacs/.doom.d/packages.el
+++ emacs/.doom.d/packages.el
@@ -1,12 +1,14 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; .doom.d/packages.el
-
+(now this is podracing!)
 ;;; Examples:
 ;; (package! some-package)
-;; (package! another-package :recipe (:host github :repo "username/repo"))
+;; (package! another-package :recspe (:host github :repo "username/repo"))
 ;; (package! builtin-package :disable t)
 ;; (package! key-chord) - allows for binding commands to combinations of key strokes 
 (package! linum-relative)
 (package! org-alert)
-(package! undo-tree-visualize)
+(package! undo-tree)
 (package! evil-snipe)
+(package! writeroom-mode) ;; removes distractions
+(package! focus) ;; focuses on current line
--- vim/.vimrc
+++ vim/.vimrc
@@ -5,9 +5,10 @@ call plug#begin('~/.vim/plugged')
 Plug 'vim-airline/vim-airline'
 Plug 'tpope/vim-surround'
 Plug 'scrooloose/nerdtree'
+Plug 'junegunn/goyo.vim'
+Plug 'junegunn/limelight.vim'
+Plug 'jceb/vim-orgmode'
 call plug#end()
 
 map <C-n> :NERDTreeToggle<CR>
-
 autocmd bufenter * if (winnr("$") == 1 && exists("b:NERDTree") && b:NERDTee.isTabTree()) | q | endif
-
